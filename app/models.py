b4c5b54948d97be83e53ec4364e30020095a1498
Chinnupinky10 <thummalapravallika4@gmail.>
2024-02-01 15:36:39 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -5,7 +5,9 @@ class Dept(models.Model):
     deptno=models.IntegerField(primary_key=True)
     dname=models.CharField(max_length=100)
     dlocation=models.CharField(max_length=100)
-
+    
+    def __str__(self):
+        return self.dname
     
     
 
@@ -18,7 +20,9 @@ class Emp(models.Model):
     sal=models.DecimalField(max_digits=10,decimal_places=2)
     comm=models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
     deptno=models.ForeignKey(Dept,on_delete=models.CASCADE)
-
+    
+    def __str__(self):
+        return self.ename
     
 
 
