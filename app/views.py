eedad11f562fa727fd944d625354d00b6632f80a
Chinnupinky10 <thummalapravallika4@gmail.>
2024-02-02 11:08:18 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -17,8 +17,24 @@ def equijoins(request):
     EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=True)
     EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=False)
     EMPOBJECTS=Emp.objects.select_related('deptno').all()[2:5:]
-    
-    
+
     d={'EMPOBJECTS':EMPOBJECTS}
     return render(request,'equijoins.html',d)
-    
+
+
+def selfjoins(request):
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(mgr__ename__contains='S')
+    EMPOBJECTS=Emp.objects.select_related('mgr').all()
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(ename__startswith='A')
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(job='MANAGER')
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(hiredate__year=2024)
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(job__endswith='k')
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(hiredate__month=1)
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(hiredate__month=1,deptno=20)
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(mgr__ename='SCOTT')
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(sal__gte=1500)
+    EMPOBJECTS=Emp.objects.select_related('mgr').filter(mgr__deptno=30)
+
+    d={'EMPOBJECTS':EMPOBJECTS}
+    return render(request,'selfjoins.html',d)
+
